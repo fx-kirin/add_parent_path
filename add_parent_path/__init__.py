c7dd82ca2d12fa901e3c82c280311ba277804b07
fx-kirin <fx.kirin@gmail.com>
2019-01-15 14:55:21 +0900

    fix bug and make it use pathlib

--- add_parent_path/__init__.py
+++ add_parent_path/__init__.py
@@ -1,34 +1,34 @@
 """add_parent_path - """
 
-__version__ = '0.1.2'
+__version__ = '0.1.3'
 __author__ = 'fx-kirin <fx.kirin@gmail.com>'
 __all__ = []
 
-import os
 import sys
 import inspect
+from pathlib import Path
 
 
 def add_parent_path(count=1):
-    caller_file_path = os.path.abspath(inspect.stack()[1].filename)
-    import_file_path = os.path.dirname(caller_file_path)
-    for _ in range(count):
-        import_file_path = os.path.dirname(import_file_path)
+    caller_file_path = Path(inspect.stack()[1].filename)
+    import_file_path = str(caller_file_path.parents[count].absolute())
 
     if import_file_path not in sys.path:
         sys.path.append(import_file_path)
-        return AddParentPath(import_file_path)
+        return AddParentPath(import_file_path, True)
     else:
-        return AddParentPath()
+        return AddParentPath(import_file_path, False)
 
 
 class AddParentPath(object):
-    def __init__(self, import_file_path):
+    def __init__(self, import_file_path, is_imported):
         self.import_file_path = import_file_path
+        self.is_imported = is_imported
 
     def __enter__(self):
         pass
 
     def __exit__(self, ex_type, ex_value, trace):
-        if self.import_file_path in sys.path:
-            sys.path.remove(self.import_file_path)
+        if self.is_imported:
+            if self.import_file_path in sys.path:
+                sys.path.remove(self.import_file_path)
